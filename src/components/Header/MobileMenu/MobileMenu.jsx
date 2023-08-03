2272a055388020598d6e35cbbb3dc883e37e11ed
Oleksandr <115045030+Oleksandr-Kharkiv@users.noreply.github.com>
2023-08-03 22:50:01 +0200

    fix close mobMenu with click on backdrop

--- src/components/Header/MobileMenu/MobileMenu.jsx
+++ src/components/Header/MobileMenu/MobileMenu.jsx
@@ -2,7 +2,7 @@ import css from './MobileMenu.module.css';
 import { useSelector } from 'react-redux';
 import { NavLink, useLocation } from 'react-router-dom';
 import sprite from '../../../images/icons.svg';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const MobileMenu = () => {
   const language = useSelector(state => state.language);
@@ -24,6 +24,15 @@ const MobileMenu = () => {
     setMobileMenuOpen(!isMobileMenuOpen);
     document.body.style.overflow = isMobileMenuOpen ? 'auto' : 'hidden';
   };
+
+  const backdropRef = useRef(null);
+
+  const handleBackdropClick = event => {
+    if (event.target === backdropRef.current) {
+      toggleMobileMenu();
+    }
+  };
+  
   useEffect(() => {
     document.body.style.overflow = 'auto';
   }, [location.pathname]);
@@ -41,31 +50,35 @@ const MobileMenu = () => {
       </button>
 
       {isMobileMenuOpen && (
-        <div className={css.backdrop} onClick={toggleMobileMenu}>
         <div
-          className={`${css.mobMenu} ${isMobileMenuOpen ? 'is-hidden' : ''}`}
+          className={css.backdrop}
+          ref={backdropRef}
+          onClick={handleBackdropClick}
         >
-          <button
-            type="button"
-            className={css.closeMobMenuBtn}
-            onClick={toggleMobileMenu}
+          <div
+            className={`${css.mobMenu} ${isMobileMenuOpen ? 'is-hidden' : ''}`}
           >
-            <svg width="40" height="40" className={css.closeMobMenuIcon}>
-              <use href={`${sprite}#close-menu`}></use>
-            </svg>
-          </button>
-          <nav className={css.mobMenuNav}>
-            <NavLink className={css.mobMenuLink} to="/startseite">
-              {mainPage}
-            </NavLink>
-            <NavLink className={css.mobMenuLink} to="/projekte">
-              {projectsPage}
-            </NavLink>
-            <NavLink className={css.mobMenuLink} to="/kontakte">
-              {contactsPage}
-            </NavLink>
-          </nav>
-        </div>
+            <button
+              type="button"
+              className={css.closeMobMenuBtn}
+              onClick={toggleMobileMenu}
+            >
+              <svg width="40" height="40" className={css.closeMobMenuIcon}>
+                <use href={`${sprite}#close-menu`}></use>
+              </svg>
+            </button>
+            <nav className={css.mobMenuNav}>
+              <NavLink className={css.mobMenuLink} to="/startseite">
+                {mainPage}
+              </NavLink>
+              <NavLink className={css.mobMenuLink} to="/projekte">
+                {projectsPage}
+              </NavLink>
+              <NavLink className={css.mobMenuLink} to="/kontakte">
+                {contactsPage}
+              </NavLink>
+            </nav>
+          </div>
         </div>
       )}
     </>
