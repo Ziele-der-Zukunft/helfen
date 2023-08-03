12fb59b137d23152e8828a8442fd4ef0629d1980
Oleksandr <115045030+Oleksandr-Kharkiv@users.noreply.github.com>
2023-08-04 00:23:33 +0200

    Update ProjectsPage.jsx

--- src/Pages/ProjectsPage/ProjectsPage.jsx
+++ src/Pages/ProjectsPage/ProjectsPage.jsx
@@ -1,9 +1,9 @@
 import Title from 'components/Title/Title';
 import Header from '../../components/Header/Header';
-import ProjectBooksText from 'Pages/ProjectsPage/ProjectBooksText/ProjectBooksText.jsx';
-import ProjectBooksGallery from './ProjectBooksGallery/ProjectBooksGallery.jsx';
-// import ProjectFoodText from 'Pages/ProjectsPage/ProjectFoodText/ProjectFoodText.jsx';
-// import ProjectFoodGallery from './ProjectFoodGallery/ProjectFoodGallery.jsx';
+// import ProjectBooksText from 'Pages/ProjectsPage/ProjectBooksText/ProjectBooksText.jsx';
+// import ProjectBooksGallery from './ProjectBooksGallery/ProjectBooksGallery.jsx';
+import ProjectFoodText from 'Pages/ProjectsPage/ProjectFoodText/ProjectFoodText.jsx';
+import ProjectFoodGallery from './ProjectFoodGallery/ProjectFoodGallery.jsx';
 import Banking from 'components/Banking/Banking';
 import Footer from 'components/Footer/Footer';
 
@@ -13,10 +13,10 @@ const ProjectPage = () => {
       <>
       <Title />
       <Header />
-      <ProjectBooksText/>
-      <ProjectBooksGallery/>
-      {/* <ProjectFoodText/> 
-      <ProjectFoodGallery/> */}
+      {/* <ProjectBooksText/>
+      <ProjectBooksGallery/> */}
+      <ProjectFoodText/> 
+      <ProjectFoodGallery/>
       <Banking/> 
       <Footer />
       </>
