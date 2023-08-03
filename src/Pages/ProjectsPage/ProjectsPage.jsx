e1b06ea9b32d9e0ad984ab912db002895330c84d
Oleksandr <115045030+Oleksandr-Kharkiv@users.noreply.github.com>
2023-08-03 23:13:02 +0200

    test

--- src/Pages/ProjectsPage/ProjectsPage.jsx
+++ src/Pages/ProjectsPage/ProjectsPage.jsx
@@ -2,10 +2,10 @@ import Banking from 'components/Banking/Banking';
 import Header from '../../components/Header/Header';
 import Footer from 'components/Footer/Footer';
 import Title from 'components/Title/Title';
-import ProjectBooksText from 'Pages/ProjectsPage/ProjectBooksText/ProjectBooksText';
-import ProjectBooksGallery from './ProjectBooksGallery/ProjectBooksGallery';
-import ProjectFoodText from 'Pages/ProjectsPage/ProjectFoodText/ProjectFoodText';
-import ProjectFoodGallery from './ProjectFoodGallery/ProjectFoodGallery';
+import ProjectBooksText from 'Pages/ProjectsPage/ProjectBooksText/ProjectBooksText.jsx';
+import ProjectBooksGallery from './ProjectBooksGallery/ProjectBooksGallery.jsx';
+import ProjectFoodText from 'Pages/ProjectsPage/ProjectFoodText/ProjectFoodText.jsx';
+import ProjectFoodGallery from './ProjectFoodGallery/ProjectFoodGallery.jsx';
 
 
 const ProjectPage = () => {
