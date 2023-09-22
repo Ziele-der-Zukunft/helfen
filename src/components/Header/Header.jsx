03256da61f64191800b6f3ce499282690e874aad
Oleksandr <115045030+Oleksandr-Kharkiv@users.noreply.github.com>
2023-09-22 12:36:02 +0200

    add @media screen 768

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,29 +2,59 @@ import css from './Header.module.css';
 import Navbar from 'components/Header/NavBar/NavBar';
 import LangBar from 'components/Header/LangBar/LangBar';
 import MobileMenu from './MobileMenu/MobileMenu';
-import { useEffect, useState } from 'react';
-import { throttle } from 'lodash';
 
 const Header = () => {
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
-
-  useEffect(() => {
-    const handleResize = throttle(() => {
-      setIsMobile(window.innerWidth <= 768);
-    }, 500);
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
 
   return (
     <div className={css.headerComponent}>
-      {isMobile ? <MobileMenu /> : <Navbar />}
+      <div className={css.buttonMobileMenu}>
+        <MobileMenu />
+      </div>
+      <div className={css.navbar}>
+        <Navbar />
+      </div>
       <LangBar />
     </div>
   );
 };
 export default Header;
+
+
+
+
+
+
+
+/*============================================================================================================*/
+
+
+// import css from './Header.module.css';
+// import Navbar from 'components/Header/NavBar/NavBar';
+// import LangBar from 'components/Header/LangBar/LangBar';
+// import MobileMenu from './MobileMenu/MobileMenu';
+// import { useEffect, useState } from 'react';
+// import { throttle } from 'lodash';
+
+// const Header = () => {
+//   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+//   useEffect(() => {
+//     const handleResize = throttle(() => {
+//       setIsMobile(window.innerWidth <= 768);
+//     }, 500);
+
+//     window.addEventListener('resize', handleResize);
+
+//     return () => {
+//       window.removeEventListener('resize', handleResize);
+//     };
+//   }, []);
+
+//   return (
+//     <div className={css.headerComponent}>
+//       {isMobile ? <MobileMenu /> : <Navbar />}
+//       <LangBar />
+//     </div>
+//   );
+// };
+// export default Header;
--- src/components/Header/MobileMenu/MobileMenu.jsx
+++ src/components/Header/MobileMenu/MobileMenu.jsx
@@ -86,3 +86,98 @@ const MobileMenu = () => {
 };
 
 export default MobileMenu;
+
+
+
+
+/*================================================================================*/
+
+
+// import css from './MobileMenu.module.css';
+// import { useSelector } from 'react-redux';
+// import { NavLink, useLocation } from 'react-router-dom';
+// import sprite from '../../../images/icons.svg';
+// import { useEffect, useRef, useState } from 'react';
+
+// const MobileMenu = () => {
+//   const language = useSelector(state => state.language);
+//   const location = useLocation();
+//   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+//   const mainPage =
+//     language === 'de' ? 'Startseite' : language === 'ua' ? 'Головна' : 'Main';
+//   const projectsPage =
+//     language === 'de' ? 'Projekte' : language === 'ua' ? 'Проєкти' : 'Projects';
+//   const contactsPage =
+//     language === 'de'
+//       ? 'Kontakte'
+//       : language === 'ua'
+//       ? 'Контакти'
+//       : 'Contacts';
+
+//   const toggleMobileMenu = () => {
+//     setMobileMenuOpen(!isMobileMenuOpen);
+//     document.body.style.overflow = isMobileMenuOpen ? 'auto' : 'hidden';
+//   };
+
+//   const backdropRef = useRef(null);
+
+//   const handleBackdropClick = event => {
+//     if (event.target === backdropRef.current) {
+//       toggleMobileMenu();
+//     }
+//   };
+  
+//   useEffect(() => {
+//     document.body.style.overflow = 'auto';
+//   }, [location.pathname]);
+
+//   return (
+//     <>
+//       <button
+//         type="button"
+//         className={css.openMobMenuBtn}
+//         onClick={toggleMobileMenu}
+//       >
+//         <svg width="40" height="30" className={css.openMobMenuIcon}>
+//           <use href={`${sprite}#open-menu`}></use>
+//         </svg>
+//       </button>
+
+//       {isMobileMenuOpen && (
+//         <div
+//           className={css.backdrop}
+//           ref={backdropRef}
+//           onClick={handleBackdropClick}
+//         >
+//           <div
+//             className={`${css.mobMenu} ${isMobileMenuOpen ? 'is-hidden' : ''}`}
+//           >
+//             <button
+//               type="button"
+//               className={css.closeMobMenuBtn}
+//               onClick={toggleMobileMenu}
+//             >
+//               <svg width="40" height="40" className={css.closeMobMenuIcon}>
+//                 <use href={`${sprite}#close-menu`}></use>
+//               </svg>
+//             </button>
+//             <nav className={css.mobMenuNav}>
+//               <NavLink className={css.mobMenuLink} to="/startseite">
+//                 {mainPage}
+//               </NavLink>
+//               <NavLink className={css.mobMenuLink} to="/projekte">
+//                 {projectsPage}
+//               </NavLink>
+//               <NavLink className={css.mobMenuLink} to="/kontakte">
+//                 {contactsPage}
+//               </NavLink>
+//             </nav>
+//           </div>
+//         </div>
+//       )}
+//     </>
+//   );
+// };
+
+// export default MobileMenu;
